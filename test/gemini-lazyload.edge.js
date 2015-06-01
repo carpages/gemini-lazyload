dd007446fead0f46e1504a15f3fec0ce00a63c44
Matt Rose <rose.matt@gmail.com>
2015-06-01 15:40:22 -0400

    Add build and edge test

---
+++ test/gemini-lazyload.edge.js
@@ -0,0 +1,20 @@
+requirejs.config({
+  baseUrl: '../',
+  paths: {
+    'underscore':                'bower_components/underscore/underscore',
+    'jquery':                    'bower_components/jquery/dist/jquery',
+    'handlebars':                'bower_components/handlebars/handlebars.runtime',
+    'jquery.boiler':             'bower_components/jquery-boiler/jquery.boiler',
+    'gemini.support':            'bower_components/gemini-support/gemini.support',
+    'gemini':                    'bower_components/gemini-loader/gemini',
+    'gemini.fold':               'bower_components/gemini-fold/gemini.fold',
+    'gemini.respond':            'bower_components/gemini-respond/gemini.respond'
+  }
+});
+
+require(['gemini', 'gemini.lazyload'], function(G){
+  G('#js-container').lazyload({
+    images: 'img.lazy',
+    bindWindow: true
+  });
+});
